b65ba3bf1dc4d8bf9f20d4c09e8a6c0ae9830aeb
Ismael CM <ismael14_16@hotmail.com>
2018-04-26 02:18:06 -0500

    Add more data when get token

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -9,47 +9,48 @@ use Firebase\JWT\JWT;
 
 class AuthController extends BaseController
 {
-	/*
-	|---------------------------------------------------------------------------------------------------
-	| User
-	|---------------------------------------------------------------------------------------------------
-	*/
-	public function token ( ServerRequestInterface $request, ResponseInterface $response, $args )
-	{
-		$result = array(
-			'status' => false,
-			'message' => '',
-		);
+    /*
+    |---------------------------------------------------------------------------------------------------
+    | User
+    |---------------------------------------------------------------------------------------------------
+    */
+    public function token(ServerRequestInterface $request, ResponseInterface $response, $args)
+    {
+        $result = array(
+            'status' => false,
+            'message' => '',
+        );
 
-		$body = $request->getParsedBody();
-		$username = $body[ 'username' ];
-		$password = $body[ 'password' ];
-        $result['body'] = $body;
-		if ( trim($username) != '' && trim($password) != '' ) {
-			try {
-				$password = hash('sha256', $body[ 'password' ]);
-				$user = User::select('id_user', 'username', 'email')->where('username', '=', $username)
-					->where('password', $password)->firstOrFail();
-				$jwt_data = $this->container->get('settings')[ 'app' ][ 'data_jwt' ];
-				$jwt_data[ 'data' ] = [
-					'username' => $user->username,
-					'email' => $user->email,
-					'id_user' => $user->id_user,
-				];
-				$jwt = JWT::encode($jwt_data, $this->container->get('settings')[ 'app' ][ 'key_jtw' ]);
-				$result[ 'status' ] = true;
-				$result[ 'token' ] = $jwt;
-				$result[ 'message' ] = 'Token generated successful';
-				return $response->withJson($result, 200);
-			} catch ( \Exception $ex ) {
-				$result[ 'message' ] = 'Not authorized';
-				$result[ 'ex' ] = $ex->getMessage();
-				return $response->withJson($result, 404);
-			}
-		} else {
-			$result[ 'message' ] = 'Please send correct data';
-			return $response->withJson($result, 500);
-		}
-	}
+        $body = $request->getParsedBody();
+        $username = $body['username'];
+        $password = $body['password'];
+
+        if (trim($username) != '' && trim($password) != '') {
+            try {
+                $password = hash('sha256', $body['password']);
+                $user = User::where('username', '=', $username)
+                    ->where('password', $password)->firstOrFail();
+                $jwt_data = $this->container->get('settings')['app']['data_jwt'];
+                $jwt_data['data'] = [
+                    'username' => $user->username,
+                    'email' => $user->email,
+                    'id_user' => $user->id_user,
+                ];
+                $jwt = JWT::encode($jwt_data, $this->container->get('settings')['app']['key_jtw']);
+                $result['status'] = true;
+                $result['token'] = $jwt;
+                $result['user'] = $user->toArray();
+                $result['message'] = 'Token generated successful';
+                return $response->withJson($result, 200);
+            } catch (\Exception $ex) {
+                $result['message'] = 'Not authorized';
+                $result['ex'] = $ex->getMessage();
+                return $response->withJson($result, 404);
+            }
+        } else {
+            $result['message'] = 'Please send correct data';
+            return $response->withJson($result, 500);
+        }
+    }
 
 }
