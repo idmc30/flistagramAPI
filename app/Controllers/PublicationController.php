752617099dbfaa2a99d5b02624eaba5ef5e6ad5e
Ismael CM <ismael14_16@hotmail.com>
2018-04-27 02:42:46 -0500

    Fixed problem whit save publication

--- app/Controllers/PublicationController.php
+++ app/Controllers/PublicationController.php
@@ -33,56 +33,60 @@ class PublicationController extends BaseController
         $location = trim($body['location']);
         $base64Photo = trim($body['dataBase64Photo']);
 
-        try {
-            $new_name_file = Helpers::makeHash($userLogged->id_user, true) . '.jpg';
-            $base64Photo = str_replace('data:image/jpeg;base64,', '', $base64Photo);
-            $base64Photo = str_replace(' ', '+', $base64Photo);
-            $data = base64_decode($base64Photo);
-            $file = Helpers::get_path_user($userLogged->id_user, $new_name_file);
-            $path = Helpers::get_path_user($userLogged->id_user);
-            if (!file_exists($path)) {
-                mkdir($path, 0700);
-                chmod($path, 0777);
-            }
-            $success = file_put_contents($file, $data);
-            $publication = new Publication();
-            $publication->id_user = $userLogged->id_user;
-            $publication->description = strip_tags($description);
-            $publication->location = strip_tags($location);
-            $publication->save();
+        if (trim($base64Photo) != '') {
+            try {
+                $new_name_file = Helpers::makeHash($userLogged->id_user, true) . '.jpg';
+                $base64Photo = str_replace(' ', '+', $base64Photo);
+                $data = base64_decode($base64Photo);
+                $file = Helpers::get_path_user($userLogged->id_user, $new_name_file);
+                $path = Helpers::get_path_user($userLogged->id_user);
+                if (!file_exists($path)) {
+                    mkdir($path, 0700);
+                    chmod($path, 0777);
+                }
+                $success = file_put_contents($file, $data);
+                $publication = new Publication();
+                $publication->id_user = $userLogged->id_user;
+                $publication->description = strip_tags($description);
+                $publication->location = strip_tags($location);
+                $publication->save();
 
-            $photo = new Photo();
-            $photo->path_photo = $new_name_file;
-            $photo->id_publication = $publication->id_publication;
-            $photo->public_path = "/api/v1/storage/pub/$publication->id_publication/image.jpg";
-            $photo->save();
-            /* Get data of publication*/
-            $publication->user;
-            $publication->photo;
-            foreach ($publication->likes as $comment) {
-                $comment->user;
-            }
-            foreach ($publication->comments as $comment) {
-                $comment->user;
-            }
-            $myLike = Like::where([
-                ['id_publication', '=', $publication->id_publication],
-                ['id_user', '=', $userLogged->id_user],
-            ])->first();
+                $photo = new Photo();
+                $photo->path_photo = $new_name_file;
+                $photo->id_publication = $publication->id_publication;
+                $photo->public_path = "/api/v1/storage/pub/$publication->id_publication/image.jpg";
+                $photo->save();
+                /* Get data of publication*/
+                $publication->user;
+                $publication->photo;
+                foreach ($publication->likes as $comment) {
+                    $comment->user;
+                }
+                foreach ($publication->comments as $comment) {
+                    $comment->user;
+                }
+                $myLike = Like::where([
+                    ['id_publication', '=', $publication->id_publication],
+                    ['id_user', '=', $userLogged->id_user],
+                ])->first();
 
-            $result['i_like_it'] = false;
+                $result['i_like_it'] = false;
 
-            if ($myLike) {
-                $result['i_like_it'] = $myLike->state == 1;
+                if ($myLike) {
+                    $result['i_like_it'] = $myLike->state == 1;
+                }
+
+                $result['status'] = true;
+                $result['message'] = "Created successful";
+                $result['publication'] = $publication;
+                return $response->withJson($result, 200);
+            } catch (\Exception $ex) {
+                $result['message'] = "Error when creating Publication";
+                $result['ex'] = $ex->getMessage();
+                return $response->withJson($result, 500);
             }
-
-            $result['status'] = true;
-            $result['message'] = "Created successful";
-            $result['publication'] = $publication;
-            return $response->withJson($result, 200);
-        } catch (\Exception $ex) {
-            $result['message'] = "Error when creating Publication";
-            $result['ex'] = $ex->getMessage();
+        } else {
+            $result['message'] = "Plase send the image";
             return $response->withJson($result, 500);
         }
     }
