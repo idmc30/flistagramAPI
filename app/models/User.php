a2aab5858420b1c1b8c9a8c7704e1a119fb20759
Ismael CM <ismael14_16@hotmail.com>
2018-04-28 00:01:43 -0500

    Clear space in User Model

--- app/models/User.php
+++ app/models/User.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 class User extends Model
 {
-
     protected $table = 'user';
     protected $fillable = array(
         "id_user",
@@ -15,23 +14,18 @@ class User extends Model
         "path_photo",
         "name_file_photo",
     );
-
     protected $primaryKey = 'id_user';
     protected $hidden = ['password', 'updated_at', 'created_at', 'email', "name_file_photo"];
-
     public function publications()
     {
         return $this->hasMany('App\Models\Publication', 'id_user', 'id_user');
     }
-
     public function followers()
     {
         return $this->hasMany('App\Models\Follow', 'id_to_follow', 'id_user');
     }
-
     public function followed()
     {
         return $this->hasMany('App\Models\Follow', 'id_follower', 'id_user');
     }
-
 }
